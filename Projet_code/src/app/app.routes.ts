f6c41050a9231e11597c58d2fb5db4d902b8e48f
ding <fffantasque@gmail.com>
2025-02-05 14:16:49 +0100

    map

--- Projet_code/src/app/app.routes.ts
+++ Projet_code/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {MiseEnPageComponent} from './mise-en-page/mise-en-page.component';
 import {LoginComponent} from './loginComponent/login.component';
+import {MapComponent} from './map/map.component';
 
 export const routes: Routes = [{ path: 'home', component: MiseEnPageComponent },
-  { path: 'login', component: LoginComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'map', component: MapComponent },
   {path: '', redirectTo: '/login', pathMatch: 'full'}];
--- Projet_code/src/app/map/map.component.ts
+++ Projet_code/src/app/map/map.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import {latLng, tileLayer} from 'leaflet';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  imports: [LeafletModule,FormsModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
 export class MapComponent {
-
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 12,
+    center: latLng(46.879966, -121.726909)
+  };
 }
